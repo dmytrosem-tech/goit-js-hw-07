69ac6630fb1d5367744fc7317cb32b1a57d4e724
Dmytro Semeruk <deemoon83@gmail.com>
2021-07-12 14:52:26 +0300

    7-8ЖЕСТЬ

--- js/task-01.js
+++ js/task-01.js
@@ -1,30 +1,45 @@
-const categoriesEl = document.querySelector('#categories');
 
-const categoriesCount = categoriesEl.children.length;
-console.log(`В списке ${categoriesCount} категории:`);
+// Мой вариант ---------------------------------------------------------------------->
+// const categoriesEl = document.querySelector('#categories');
 
-const categoriesValuesCount = categoriesEl.querySelectorAll("ul")
+// const categoriesCount = categoriesEl.children.length;
+// console.log(`В списке ${categoriesCount} категории:`);
+
+// const categoriesValuesCount = categoriesEl.querySelectorAll("ul")
 
 
-// Находим первую категорию и количество в ней элементов---->
-const categorieFirst = categoriesEl.firstElementChild;
-const categorieFirstTitle = categorieFirst.firstElementChild.textContent;
-const categorieFirstCountIn = categoriesValuesCount[0].children.length;
-console.log(`Категория: ${categorieFirstTitle}`);
-console.log(`Количество элементов: ${categorieFirstCountIn}`);
+// // Находим первую категорию и количество в ней элементов---->
+// const categorieFirst = categoriesEl.firstElementChild;
+// const categorieFirstTitle = categorieFirst.firstElementChild.textContent;
+// const categorieFirstCountIn = categoriesValuesCount[0].children.length;
+// console.log(`Категория: ${categorieFirstTitle}`);
+// console.log(`Количество элементов: ${categorieFirstCountIn}`);
 
-// Находим вторую категорию и количество в ней элементов---->
-const categorieSecond = categoriesEl.children[1];
-const categorieSecondTitle = categorieSecond.firstElementChild.textContent;
-const categorieSecondCountIn = categoriesValuesCount[1].children.length;
-console.log(`Категория: ${categorieSecondTitle}`);
-console.log(`Количество элементов: ${categorieSecondCountIn}`);
+// // Находим вторую категорию и количество в ней элементов---->
+// const categorieSecond = categoriesEl.children[1];
+// const categorieSecondTitle = categorieSecond.firstElementChild.textContent;
+// const categorieSecondCountIn = categoriesValuesCount[1].children.length;
+// console.log(`Категория: ${categorieSecondTitle}`);
+// console.log(`Количество элементов: ${categorieSecondCountIn}`);
 
-// Находим третью категорию и количество в ней элементов---->
-const categorieThird = categoriesEl.children[2];
-const categorieThirdTitle = categorieThird.firstElementChild.textContent;
-const categorieThirdCountIn = categoriesValuesCount[2].children.length;
-console.log(`Категория: ${categorieThirdTitle}`);
-console.log(`Количество элементов: ${categorieThirdCountIn}`);
+// // Находим третью категорию и количество в ней элементов---->
+// const categorieThird = categoriesEl.children[2];
+// const categorieThirdTitle = categorieThird.firstElementChild.textContent;
+// const categorieThirdCountIn = categoriesValuesCount[2].children.length;
+// console.log(`Категория: ${categorieThirdTitle}`);
+// console.log(`Количество элементов: ${categorieThirdCountIn}`);
+
+// Вариант с подсказки ---------------------------------------------------------------->
+const categoriesEl = document.querySelector("#categories");
+console.log(`В списке ${categoriesEl.children.length} категории:`);
+
+const headline = document.querySelectorAll("h2");
+
+const names = headline.forEach((name) => {
+  console.log(`Категория: ${name.outerText}`);
+  console.log(
+    `Количество элементов: ${name.parentNode.querySelectorAll("li").length}`
+  );
+});
 
 
--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,68 @@
+// получаем ссылки-------------------------------------------------------------->
 const refs = {
   controlsRef: document.querySelector("#controls"),
   boxesRef: document.querySelector("#boxes"),
   inputRef: document.querySelector("input"),
 };
+const btn = document.querySelector("#boxes");
 const btnRenderRef = refs.controlsRef.children[1];
 const btnDestroyRef = refs.controlsRef.lastElementChild;
-const createDiv = document.createElement("div");
 
+// Вешаем слушатели------------------------------------------------------------->
 refs.controlsRef.addEventListener("input", onValueChange);
+btnRenderRef.addEventListener("click", onRenderBtnClick);
+btnDestroyRef.addEventListener("click", onDestrBtnClick);
 
-function onValueChange(event) {
-  console.log(Number(event.currentTarget.firstElementChild.value));
+// Это массив элементов который будет добавлятся в верстку после распыления----->
+let divArr = [];
+
+// Сначала делаем функции создания и добавления ящиков-------------------------->
+function onRenderBtnClick(event) {
+  createBoxes(refs.inputRef.value);
 }
 
 function createBoxes(amount) {
-  refs.boxesRef.appendChild(createDiv);
+  // console.log(amount);
+  let inputValue = amount;
+  let divWidth = 20;
+  let divHeight = 20;
+
+  for (let i = 0; i < inputValue; i += 1) {
+    const boxesDiv = document.createElement("div");
+    boxesDiv.classList.add("boxes__div");
+    boxesDiv.style.backgroundColor = `rgb(${getRandomColor(
+      0,
+      255
+    )}, ${getRandomColor(0, 255)}, ${getRandomColor(0, 255)})`;
+    divWidth += 10;
+    boxesDiv.style.width = `${divWidth}px`;
+    divHeight += 10;
+    boxesDiv.style.height = `${divHeight}px`;
+    divArr.push(boxesDiv);
+  }
+  refs.boxesRef.append(...divArr);
+}
+
+// Теперь функции очищения родительского элемента от того что мы наваяли-------->
+function onDestrBtnClick(event) {
+  destroyBoxes();
+}
+
+function destroyBoxes() {
+  //  refs.boxesRef.children.remove(); / ТУТ ДОЛЖНО ОТРАБОТАТЬ - НО НЕТ((
+  console.log(refs.boxesRef.children);
+  divArr.splice(0, 1);
+  console.log(divArr);
+  refs.boxesRef.append(...divArr);
+}
+
+
+// Тут вынимаем значение инпута при вводе, ----------------------------------->
+function onValueChange(event) {
+  return Number(event.currentTarget.firstElementChild.value);
+}
+
+// Функция рандомного цвета--------------------------------------------------->
+function getRandomColor(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
 }
