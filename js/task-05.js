9e2d58386f3a3ef5bf4def1517747793ee3c7ea4
Dmytro Semeruk <deemoon83@gmail.com>
2021-07-12 15:59:31 +0300

    7-fin2

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,10 @@ const refs = {
   outputEl: document.querySelector("#name-output"),
 };
 
-refs.inputEl.addEventListener("input", (event) => 
-   refs.outputEl.textContent = event.currentTarget.value);
-  
-   if (refs.inputEl.attributes()) {
-       refs.outputEl.textContent = 'lol'
-   }
+refs.inputEl.addEventListener("input", (event) => {
+  event.currentTarget.value.trim() 
+  ? refs.outputEl.textContent = event.currentTarget.value
+  : refs.outputEl.textContent = 'незнакомец';
+});
+
+
