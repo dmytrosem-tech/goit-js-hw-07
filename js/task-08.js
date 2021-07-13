5b588c5aef1dbb1a5d4425bbc85eb4a43543c705
Dmytro Semeruk <deemoon83@gmail.com>
2021-07-13 09:40:12 +0300

    7-8-arrFixed

--- js/task-08.js
+++ js/task-08.js
@@ -1,63 +1,65 @@
 // получаем ссылки-------------------------------------------------------------->
 const refs = {
-  controlsRef: document.querySelector("#controls"),
-  boxesRef: document.querySelector("#boxes"),
-  inputRef: document.querySelector("input"),
-};
-const btn = document.querySelector("#boxes");
-const btnRenderRef = refs.controlsRef.children[1];
-const btnDestroyRef = refs.controlsRef.lastElementChild;
+  controlsRef: document.querySelector('#controls'),
+  boxesRef: document.querySelector('#boxes'),
+  inputRef: document.querySelector('input'),
+}
+const btn = document.querySelector('#boxes')
+const btnRenderRef = refs.controlsRef.children[1]
+const btnDestroyRef = refs.controlsRef.lastElementChild
 
 // Вешаем слушатели------------------------------------------------------------->
-refs.controlsRef.addEventListener("input", onValueChange);
-btnRenderRef.addEventListener("click", onRenderBtnClick);
-btnDestroyRef.addEventListener("click", onDestrBtnClick);
+refs.controlsRef.addEventListener('input', onValueChange)
+btnRenderRef.addEventListener('click', onRenderBtnClick)
+btnDestroyRef.addEventListener('click', onDestrBtnClick)
 
 // Это массив элементов который будет добавлятся в верстку после распыления----->
-let divArr = [];
+let divArr = []
 
 // Сначала делаем функции создания и добавления ящиков-------------------------->
 function onRenderBtnClick(event) {
-  createBoxes(refs.inputRef.value);
+  createBoxes(refs.inputRef.value)
 }
 
 function createBoxes(amount) {
   // console.log(amount);
-  let inputValue = amount;
-  let divWidth = 20;
-  let divHeight = 20;
+  let inputValue = amount
+  let divWidth = 20
+  let divHeight = 20
 
   for (let i = 0; i < inputValue; i += 1) {
-    const boxesDiv = document.createElement("div");
-    boxesDiv.classList.add("boxes__div");
-    boxesDiv.style.backgroundColor = `rgb(${getRandomColor(
+    const boxesDiv = document.createElement('div')
+    boxesDiv.classList.add('boxes__div')
+    boxesDiv.style.backgroundColor = `rgb(${getRandomColor(0, 255)}, ${getRandomColor(0, 255)}, ${getRandomColor(
       0,
-      255
-    )}, ${getRandomColor(0, 255)}, ${getRandomColor(0, 255)})`;
-    divWidth += 10;
-    boxesDiv.style.width = `${divWidth}px`;
-    divHeight += 10;
-    boxesDiv.style.height = `${divHeight}px`;
-    divArr.push(boxesDiv);
+      255,
+    )})`
+    divWidth += 10
+    boxesDiv.style.width = `${divWidth}px`
+    divHeight += 10
+    boxesDiv.style.height = `${divHeight}px`
+    divArr.push(boxesDiv)
   }
-  refs.boxesRef.append(...divArr);
+  refs.boxesRef.append(...divArr)
+  console.log(divArr)
 }
 
 // Теперь функции очищения родительского элемента от того что мы наваяли-------->
 function onDestrBtnClick(event) {
-  destroyBoxes();
+  destroyBoxes()
 }
 
 function destroyBoxes() {
-   refs.boxesRef.innerHTML = ''; 
+  refs.boxesRef.innerHTML = ''
+  divArr = []
 }
 
 // Тут вынимаем значение инпута при вводе, ----------------------------------->
 function onValueChange(event) {
-  return Number(event.currentTarget.firstElementChild.value);
+  return Number(event.currentTarget.firstElementChild.value)
 }
 
 // Функция рандомного цвета--------------------------------------------------->
 function getRandomColor(min, max) {
-  return Math.ceil(Math.random() * (max - min) + min);
+  return Math.ceil(Math.random() * (max - min) + min)
 }
