6668573048402a01960fca2b78faa3bed2a80c7d
Dmytro Semeruk <deemoon83@gmail.com>
2021-07-15 13:09:14 +0300

    2-fix

--- js/task-02.js
+++ js/task-02.js
@@ -1,13 +1,19 @@
 const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы']
 
 // Перебираем массив с названиями лишек------------------>
-const elArr = []
-ingredients.forEach(ingredient => {
-  const ingredientEl = document.createElement('li')
-  ingredientEl.textContent = ingredient
-  elArr.push(ingredientEl)
+// const elArr = []
+// ingredients.forEach(ingredient => {
+//   const ingredientEl = document.createElement('li')
+//   ingredientEl.textContent = ingredient
+//   elArr.push(ingredientEl)
+// })
+
+const elsArr = ingredients.map(ingredient => {
+  const el = document.createElement('li')
+  el.textContent = ingredient
+  return el
 })
 
 // Добавляем массив лишек в DOM----------------------------------->
 const parentEl = document.querySelector('#ingredients')
-parentEl.append(...elArr)
+parentEl.append(...elsArr)
