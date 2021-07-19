dcd252dee2def1121f3e145e27cfb19747ae815b
Dmytro Semeruk <deemoon83@gmail.com>
2021-07-19 10:51:13 +0300

    some fix

--- js/task-07.js
+++ js/task-07.js
@@ -1,19 +1,27 @@
+// const refs = {
+//     inputWormRef: document.querySelector('#font-size-control'),
+//     spanRef: document.querySelector('#text')
+// }
+
+// refs.spanRef.style.fontSize = '20px';
+// refs.inputWormRef.setAttribute('value', '20');
+// refs.inputWormRef.setAttribute('min', '1');
+// refs.inputWormRef.setAttribute('max', '40');
+// refs.inputWormRef.setAttribute('step', '1');
+
+// refs.inputWormRef.addEventListener('input', onValueChange);
+
+// function onValueChange (event) {
+//     const value = event.currentTarget.value;
+//     refs.spanRef.style.fontSize = `${value}px`;
+// }
+
 const refs = {
-    inputWormRef: document.querySelector('#font-size-control'),
-    spanRef: document.querySelector('#text')
+  inputRef: document.querySelector('#font-size-control'),
+  textRef: document.querySelector('#text'),
 }
-
-refs.spanRef.style.fontSize = '20px';
-refs.inputWormRef.setAttribute('value', '20');
-refs.inputWormRef.setAttribute('min', '1');
-refs.inputWormRef.setAttribute('max', '40');
-refs.inputWormRef.setAttribute('step', '1');
-
-refs.inputWormRef.addEventListener('input', onValueChange);
-
-function onValueChange (event) {
-    const value = event.currentTarget.value;
-    refs.spanRef.style.fontSize = `${value}px`;
+function onChangeFontSize(event) {
+  const fontSize = +event.currentTarget.value
+  refs.textRef.style.fontSize = `${fontSize}px`
 }
-
-
+refs.inputRef.addEventListener('input', onChangeFontSize)
